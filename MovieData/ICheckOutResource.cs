4222e1928d1b21ed9728d47124a84ab160bb763d
shaneswezey <shanedswezey@gmail.com>
2018-09-02 20:16:38 -0700

    Added Checkout Interface

---
+++ MovieData/ICheckOutResource.cs
@@ -0,0 +1,28 @@
+﻿using MovieData.DataModels;
+using System.Collections.Generic;
+
+namespace MovieData
+{
+    public interface ICheckOutResource
+    {
+        IEnumerable<RentalCheckout> GetAllCheckouts();
+        RentalCheckout GetCheckout(int id);
+
+        IEnumerable<RentalCheckoutHistory> GetAllRentalCheckoutHistories();
+        RentalCheckoutHistory GetRentalCheckoutHistory(int id);
+
+        void PlaceHold(int renterId, int movieAssestId);
+        void Checkout(int renterId, int movieAssestId);
+        void Checkin(int movieAssestId);
+
+        RentalCheckout GetLatestCheckout(int renterId);
+
+        int GetNumberOfCopies(int movieAssestId, string diskFormat);
+        bool IsCheckout(int movieAssestId);
+
+        IEnumerable<Hold> GetCurrentHolds(int movieAssestId);
+
+        void MarkLost(int movieAssestId);
+        void MarkFound(int movieAssestId);
+    }
+}
