b3e87e800579ec326f699699c2bebf3b9fa9072a
shaneswezey <shanedswezey@gmail.com>
2018-09-15 11:44:01 -0700

    New model for user current rental information

---
+++ MovieRentalMVC/Models/User/UserDetailModel.cs
@@ -0,0 +1,11 @@
+﻿using MovieData.DataModels;
+using System.Collections.Generic;
+
+namespace MovieRentalMVC.Models.User
+{
+    public class UserDetailModel
+    {
+        public IEnumerable<Hold> Holds { get; set; }
+        public IEnumerable<RentalCheckout> Checkouts { get; set; }
+    }
+}
