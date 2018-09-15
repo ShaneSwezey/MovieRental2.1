ada0c5192766bbf9badfa18a997bd824932c36d0
shaneswezey <shanedswezey@gmail.com>
2018-09-15 13:18:08 -0700

    Changed UserDetailModel from IEnumerable<RentalCheckout> to IEnumerable<CheckoutModel>

--- MovieRentalMVC/Models/User/UserDetailModel.cs
+++ MovieRentalMVC/Models/User/UserDetailModel.cs
@@ -6,6 +6,6 @@ namespace MovieRentalMVC.Models.User
     public class UserDetailModel
     {
         public IEnumerable<Hold> Holds { get; set; }
-        public IEnumerable<RentalCheckout> Checkouts { get; set; }
+        public IEnumerable<CheckoutModel> Checkouts { get; set; }
     }
 }
