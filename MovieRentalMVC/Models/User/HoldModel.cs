b6e353189d95fe1a535f17e51d2692039c9beb49
shaneswezey <shanedswezey@gmail.com>
2018-09-15 14:02:28 -0700

    Added hold model for view

---
+++ MovieRentalMVC/Models/User/HoldModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MovieRentalMVC.Models.User
+{
+    public class HoldModel
+    {
+        public string Title { get; set; }
+        public DateTime HoldDate { get; set; }
+        public string DiskType { get; set; }
+    }
+}
